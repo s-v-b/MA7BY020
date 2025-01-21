13d3b3bc22b2bbd4afe138f88e2fd571c04b2297
Stéphane Boucheron <stephane.v.boucheron@gmail.com>
2025-01-21 15:44:36 +0100

    shifting slides to reveal

--- _qrender.sh
+++ _qrender.sh
@@ -1,4 +1,4 @@
 quarto render --to html  --no-cache
-# quarto render slides --profile slides --no-clean
+quarto render core/slides --profile slides --no-clean
 quarto render core/labs-solutions  --profile solution --no-clean
 # quarto render exams  --profile solution --no-clean
